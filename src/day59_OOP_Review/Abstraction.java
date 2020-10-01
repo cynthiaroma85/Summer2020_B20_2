f019aed1b4449e569f3ab9320deed9876db1b703
cynthiaroma85 <cynthiaroma85@gmail.com>
2020-09-30 22:22:26 -0400

    update

--- src/day59_OOP_Review/Abstraction.java
+++ src/day59_OOP_Review/Abstraction.java
@@ -2,8 +2,11 @@ package day59_OOP_Review;
 // hidding implementacion, only method, meant to be OVERRIDEN, meant to be extended
 // abstract METHOD, no body
 // like any other class: static, instance, constructor ,static block
-// abstract CLASS: is not concrete so cannot create object, they MUST implement abstract method if super class
-// Interface: its NOT a class! Just especify behavior, Interfcace can be implemented to the SUB class
+// abstract CLASS: is not concrete so cannot create object,
+//              SUBcLASS MUST implement ALL abstract method from super class
+
+// Interface: its NOT a class! Just especify CERTAIN behavior,
+//             Interfcace can be implemented to the SUB class only if NEEDED
 // class can Implement multiple INTERFACE, but can only Extend 1 Class
 // no object means no INSTANCE ( method or variable, no constructor either)
 // by default public static and final on abstract method
@@ -16,13 +19,26 @@ interface X{  // cannot be final
 
                     int a = 100;  // in Interface by default is public static, Not  instance
                     static int b = 20; //
+
+    //  static { }
 }
 
 
-abstract class Y{
+abstract  class Y{
+    int a;
+    static int b;
+
     public abstract void method1();
-}
 
+    Y(){
+
+    }
+
+    static{
+
+    }
+
+}
 
 
 
